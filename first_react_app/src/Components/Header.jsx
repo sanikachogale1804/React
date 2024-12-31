5623338930efb2753f26d54edcd07c64a9cc149d
unknown <chogalesanika8@gmail.com>
2024-12-31 09:01:08 +0530

    first commit

--- first_react_app/src/Components/Header.jsx
+++ first_react_app/src/Components/Header.jsx
@@ -1,8 +1,16 @@
 function Header()
 {
     let counter="riya";
+    let student={
+        id:101,
+        name:"nisha"
+    }
     return(
+        <div>
         <h1>Header Componenet {counter}</h1>
+        <p>Student Id: {student.id}</p>
+        <p>Student Name: {student.name}</p>
+        </div>
     );
 }
 
