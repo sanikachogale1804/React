13b4ab33ad0fcd37837b7dc53caba014ce620c9a
unknown <chogalesanika8@gmail.com>
2025-01-01 07:49:08 +0530

    initial

---
+++ first_react_app/src/Components/Demo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import {Component} from "react"; aisa bhi lhikh sakte hai
+
+//class ko componenet banana hai to React ko inmport karna padega
+//React ke andar componenet name ka class hai and usko inherit karna padega
+//render method html return karegi
+class Demo extends React.Component
+{
+   render()
+   {
+     return(
+        <div>
+            <h1>Class Based Component</h1>
+        </div>
+     );
+   }
+}
