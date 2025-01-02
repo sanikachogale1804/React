1bde788af6d73dfa8902b206abbbad77bf7645a1
unknown <chogalesanika8@gmail.com>
2025-01-02 07:19:03 +0530

    git commit -m

--- first_react_app/src/Components/Counter.jsx
+++ first_react_app/src/Components/Counter.jsx
@@ -1,2 +1,14 @@
 //counter liya jata hai to understand how react is behaviour
 
+function Counter()
+{
+    return(
+        <div>
+            <button>Decrement</button>
+            <h1>0</h1>
+            <button>Increment</button>
+        </div>
+    );
+}
+
+export default Counter;
