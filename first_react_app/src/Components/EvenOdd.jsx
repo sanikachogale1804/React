7860486690f9ce9a0933e02dea71dca16967c2b5
unknown <chogalesanika8@gmail.com>
2025-01-02 08:29:05 +0530

    initial

--- first_react_app/src/Components/EvenOdd.jsx
+++ first_react_app/src/Components/EvenOdd.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function EvenOdd()
 {
     let[inputValue,setInputValue]=useState(1)
     let[ans,setAnswer]=useState("odd");
+
+    useEffect(()=>{
+        //anne vala number string format me bhi ho sakta hai is liye usko convert kiya
+       let v=parseInt(inputValue);
+        if(v%2==0)
+           setAnswer("Even")
+        else
+           setAnswer("Odd")
+      
+        // console.log("EvenOdd")
+    },[inputValue])
     return(
         <div>
              {/* js ka event hai onchange matlab input ki value change kardi */}
@@ -11,7 +22,7 @@ function EvenOdd()
              {/* side effect matlab ek element ke change hone ke baad dusra element change ho */}
             <input type="number" value={inputValue} onChange={(e)=>{
                 setInputValue(e.target.value);
-                console.log(inputValue);
+                // console.log(inputValue);
                 }}/>
 
                 <h1>{ans}</h1>
