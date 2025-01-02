555d78a56f8b2df2992cdbc42d962fd1e3ba7f2d
unknown <chogalesanika8@gmail.com>
2025-01-02 07:31:59 +0530

    initial

--- first_react_app/src/Components/Counter.jsx
+++ first_react_app/src/Components/Counter.jsx
@@ -7,8 +7,8 @@ function Counter()
     //use state array retunr karta hai and hum usko destruct hum karte hai
     //0 count me store ho gaya hai and hum setCount ki madat se increment and decrement karenge
     let[count,setCount]=useState(0);
-    const increment=()=>{};
-    const decrement=()=>{};
+    const increment=()=>{setCount(count+1)};
+    const decrement=()=>{setCount(count-1)};
     return(
         <div>
             <button onClick={decrement}>Decrement</button>
