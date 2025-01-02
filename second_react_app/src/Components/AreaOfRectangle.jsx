8cb67fa2e97392695060f8ea2f370ce417749394
unknown <chogalesanika8@gmail.com>
2025-01-02 22:31:02 +0530

    initial

--- second_react_app/src/Components/AreaOfRectangle.jsx
+++ second_react_app/src/Components/AreaOfRectangle.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function AreaOfRectangle()
 {
     let[length,setLength]=useState(20);
+    let[width,setWidth]=useState(10);
     return(
       <div>
         <input type="number" value={length}/>
