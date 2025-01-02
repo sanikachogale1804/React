5332ad360fedfc7d98f03ed145239a45a3e04704
unknown <chogalesanika8@gmail.com>
2025-01-02 22:29:00 +0530

    initial

--- second_react_app/src/App.js
+++ second_react_app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ChangeName from './Components/ChangeName';
+import AreaOfRectangle from './Components/AreaOfRectangle';
 
 function App() {
   return (
     <div className="App">
-      <ChangeName/>   
+      <ChangeName/>
+      <AreaOfRectangle/>   
     </div>
   );
 }
--- second_react_app/src/Components/AreaOfRectangle.jsx
+++ second_react_app/src/Components/AreaOfRectangle.jsx
@@ -1,7 +1,11 @@
 function AreaOfRectangle()
 {
+    
     return(
-
+      <div>
+        <input type="number" value={length}/>
+        <input type="number" name={width}/>
+      </div>
     );
 }
 export default AreaOfRectangle;
