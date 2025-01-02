dc96afb2608400f5a82caa7bd0894c5e089576ab
unknown <chogalesanika8@gmail.com>
2025-01-02 08:04:33 +0530

    initial

--- first_react_app/src/Components/EvenOdd.jsx
+++ first_react_app/src/Components/EvenOdd.jsx
@@ -3,15 +3,18 @@ import { useState } from "react";
 function EvenOdd()
 {
     let[inputValue,setInputValue]=useState(1)
-   
+    let[ans,setAnswer]=useState("odd");
     return(
         <div>
              {/* js ka event hai onchange matlab input ki value change kardi */}
              {/* onchange karenge to ye arrow function call hoga and e me uska objcet store hoga */}
+             {/* side effect matlab ek element ke change hone ke baad dusra element change ho */}
             <input type="number" value={inputValue} onChange={(e)=>{
                 setInputValue(e.target.value);
                 console.log(inputValue);
                 }}/>
+
+                <h1>{ans}</h1>
         </div>
     );
 }
