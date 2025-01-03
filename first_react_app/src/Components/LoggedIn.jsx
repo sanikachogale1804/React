ff49f4befd646f83f102f7aab614f219593a0343
unknown <chogalesanika8@gmail.com>
2025-01-03 08:09:25 +0530

    initial

--- first_react_app/src/Components/LoggedIn.jsx
+++ first_react_app/src/Components/LoggedIn.jsx
@@ -5,7 +5,11 @@ function LoggedIn()
     let[loggedIn,setLoggedIn]=useState(false)
     return(
        <div>
-        {loggedIn?<h1>Welcome user</h1>:<button>Log IN</button>}
+        {loggedIn?
+        <h1>Welcome user</h1>:
+        <button onClick={()=>{
+            setLoggedIn(true);
+        }}>Log IN</button>}
        </div>
     );
 }
