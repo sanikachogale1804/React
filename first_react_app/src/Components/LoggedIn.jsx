976d9febc1707dff92a262a6cc1d6ee824604530
unknown <chogalesanika8@gmail.com>
2025-01-03 08:00:48 +0530

    initial

--- first_react_app/src/Components/LoggedIn.jsx
+++ first_react_app/src/Components/LoggedIn.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 function LoggedIn()
 {
-    let[loggedIn,setLoggedIn]=useState("false")
+    let[loggedIn,setLoggedIn]=useState(false)
     return(
        <div>
-
+        {loggedIn?<h1>Welcome user</h1>:<button>Log IN</button>}
        </div>
     );
 }
