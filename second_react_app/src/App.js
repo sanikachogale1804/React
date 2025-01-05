4fa68ec8c4c16aa822273d47502040acb703c154
unknown <chogalesanika8@gmail.com>
2025-01-05 18:20:19 +0530

    initial

--- second_react_app/src/App.js
+++ second_react_app/src/App.js
@@ -5,11 +5,12 @@ import AreaOfRectangle from './Components/AreaOfRectangle';
 import Header from './Components/Header';
 import ChangeYear from './Components/ChangeYear';
 import EvenOdd from './Components/EvenOdd';
-
+import AreaOfCircle from './Components/AreaOfCircle';
 
 function App() {
   return (
     <div className="App">
+      <AreaOfCircle/>
       <EvenOdd/>
       <ChangeYear/>
       <Header/>
--- second_react_app/src/Components/AreaOfCircle.jsx
+++ second_react_app/src/Components/AreaOfCircle.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 function AreaOfCircle()
 {
     let[radius,setRadius]=useState(2);
-    
+    let[ans,setArea]=useState(3.14*radius);
     return(
         <div>
-
+            <input type="number" value={radius} onChange={()=>{setRadius(e.target.value)}}/>
         </div>
     );
 }
+export default AreaOfCircle;
