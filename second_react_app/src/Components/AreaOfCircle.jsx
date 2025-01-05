39cab742e08f4d885e2390492f6472a1108771b8
unknown <chogalesanika8@gmail.com>
2025-01-05 18:15:03 +0530

    initial

---
+++ second_react_app/src/Components/AreaOfCircle.jsx
@@ -0,0 +1,8 @@
+function AreaOfCircle()
+{
+    return(
+        <div>
+            
+        </div>
+    );
+}
--- second_react_app/src/Components/EvenOdd.jsx
+++ second_react_app/src/Components/EvenOdd.jsx
@@ -4,20 +4,17 @@ function EvenOdd()
 {
     let[inputValue,setInputValue]=useState(1)
     let[ans,setAnswer]=useState("odd")
+
     useEffect(()=>{
         let v=parseInt(inputValue)
         if(v%2==0)
-        {
            setAnswer("Even")
-        }
         else
-        {
             setAnswer("odd")
-        }
     },[inputValue])
     return(
         <div>
-           <input type="text" value={inputValue} onChange={(e)=>{
+           <input type="number" value={inputValue} onChange={(e)=>{
                 setInputValue(e.target.value)
            }} />
         </div>
