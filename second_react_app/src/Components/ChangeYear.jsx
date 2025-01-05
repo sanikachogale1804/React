980c2d23a6774fac8e56de3b04cbc779b057bbb0
unknown <chogalesanika8@gmail.com>
2025-01-05 17:51:56 +0530

    initial

--- second_react_app/src/Components/ChangeYear.jsx
+++ second_react_app/src/Components/ChangeYear.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function ChangeYear(){
+
+    let[year,changeYear]=useState(2025)
     return(
         <div>
-            
+            <h1>{changeYear}</h1>
         </div>
     );
 }
+
+export default ChangeYear;
