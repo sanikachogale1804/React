5a6803441fa1dc67c1c9d48b74ba2a9052cc4053
unknown <chogalesanika8@gmail.com>
2025-01-05 18:09:02 +0530

    initial

--- second_react_app/src/Components/EvenOdd.jsx
+++ second_react_app/src/Components/EvenOdd.jsx
@@ -1,13 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function EvenOdd()
 {
     let[inputValue,setInputValue]=useState(1)
     let[ans,setAnswer]=useState("odd")
+    useEffect(()=>{
+        let v=parseInt(inputValue)
+        if(v%2==0)
+        {
+           setAnswer("Even")
+        }
+        else
+        {
+            setAnswer("odd")
+        }
+    },[inputValue])
     return(
         <div>
-           <h1>{inputValue}</h1>
-           <h1>{ans}</h1>
+           <h1></h1>
         </div>
     );
 }
