9c63078483f586210fb90ee6451deb29707c4ae5
unknown <chogalesanika8@gmail.com>
2025-01-07 08:10:38 +0530

    initial

--- third_app/src/App.js
+++ third_app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import About from './Components/About';
+import Contact from './Components/Contact';
 
 //is function me jo bhi hai usko hum variable me store karte hai
 const router=createBrowserRouter([
@@ -12,13 +13,20 @@ const router=createBrowserRouter([
   }
 ])
 
+const router2=createBrowserRouter([
+  {
+    path:"/contact",
+    element:<Contact/>
+  }
+])
 function App() {
   return (
     <div >
       {/* <Navbar/> */}
 
       {/* without this we can not work */}
-      <RouterProvider router={router}/>   
+      <RouterProvider router={router}/> 
+      <RouterProvider router={router2}/>  
     </div>
   );
 }
