2d6bef14dca224bf4404a36f3d8b30aa68a99b94
unknown <chogalesanika8@gmail.com>
2025-01-10 09:40:02 +0530

    initial

---
+++ ecommerce-react-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+// this function used to create routes
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Product from './Components/Products/Product';
+import Navbar from './Components/Products/Navbar';
+
+
+//creating routs
+//createBrowserRouter needs an array (array of objects(routes))
+const routes=createBrowserRouter([
+{
+  path:"/products",
+  element:<><Navbar/> <Product/></>
+},
+{
+  path:"/",
+  element:<Navbar/>
+}
+
+])
+function App() {
+  return (
+    <div >
+       <RouterProvider router={routes}/>
+    </div>
+  );
+}
+
+export default App;
--- third_app/src/Components/GithubCard.jsx
+++ third_app/src/Components/GithubCard.jsx
@@ -1,31 +1,67 @@
 import React, { useEffect, useState } from 'react'
 
 function GithubCard() {
-    let [githubData,setGithubData]= useState({});
-    useEffect(()=>{
+    let [githubData, setGithubData] = useState({});
+    let [repositories, setrepositories] = useState([])
+    useEffect(() => {
 
         fetch("https://api.github.com/users/sanikachogale1804")
-        .then(data=>data.json())  //json again return promise ka object
-        .then(data=>
-            {
+            .then(data => data.json())  //json again return promise ka object
+            .then(data => {
                 console.log(data);
                 setGithubData(data);
-                //console.log(githubData)
-             })
+                //Repositories
+                fetch(data.repos_url)
+                    .then(repos => repos.json())
+                    .then(repos => {
+                        //console.log("Repos "+repos)
+                        setrepositories(repos)//whatever repos i fetched that saved setrepositories 
 
-    },[])
+                    })
+            })
+
+    }, [])
     return (
         <div>
+            {/* Profile Start */}
             <h1>GitHub Profile</h1>
             <div className="card github-card" >
                 <img src={githubData.avatar_url} className="card-img-top" alt="..." />
                 <div className="card-body">
-                   
+
                     <p className="card-text">Name: {githubData.name}</p>
                     <p className="card-text">Login: {githubData.login}</p>
-                    <a href={githubData.html_url}>Click to visit profile</a>
+                    <a href={githubData.html_url}>Click to visit gitHub profile</a>
                 </div>
             </div>
+            {/* Profile:end */}
+
+            {/* Repositories:start */}
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Repository Name</th>
+                        <th scope="col">Repository Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {/* we are gonna write js code */}
+                    { repositories.map((repoobj)=>{
+                        return (
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>  
+                        </tr>
+                        )
+                    })};
+
+                   
+                    
+                </tbody>
+            </table>
+
+            {/* Repositories:end */}
         </div>
     )
 }
